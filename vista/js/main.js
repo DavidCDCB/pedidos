7a451d97ed1fdcd112269cc9c7532cdddd27bd60
DavidCDCB <cristian-cb1@hotmail.com>
2021-02-07 14:15:31 -0500

    alert

--- vista/js/main.js
+++ vista/js/main.js
@@ -4,11 +4,24 @@ import dom from './dom-pruebas.js';//clase estatica
 let productos = ["chicle","paleta","gaseosa"];
 let mesas = ["1","2","3","4","5","6","7"];
 
+let asignaciones ={
+
+};
+
 ((doc, win) => {
 	
 	doc.addEventListener('DOMContentLoaded',event => {
+		document.getElementById("alert").style.display = "none";
+
+		dom.getElemento("agregar").addEventListener('click', event => {
+			asignar(dom.getElemento("mesas").value,dom.getElemento("productos").value);
+		});
+
 		setItems(doc.querySelector("#productos"),productos);
 		setItems(doc.querySelector("#mesas"),mesas);
+
+		preparacionAsignaciones(asignaciones,mesas);
+		
 	});
 
 	let setItems=(elemento,listado)=>{
@@ -17,8 +30,27 @@ let mesas = ["1","2","3","4","5","6","7"];
 			option.text = iterator;
 			elemento.add(option);
 		}
+
 	}
 
+	let asignar=(mesa,producto)=>{
+		asignaciones[mesa].push(producto);
+
+		console.log(asignaciones);
+
+		dom.getElemento("texto").innerText= producto+" agregado correctamente";
+
+		document.getElementById("alert").style.display = "block";
+		window.setTimeout(function () {
+			document.getElementById("alert").style.display = "none";
+		}, 3000);
+	}
+	
+	let preparacionAsignaciones=(dic,lista)=>{
+		for (const iterator of lista) {
+			dic[iterator] = [];
+		}
+	}
 
 })(document, window);
 
