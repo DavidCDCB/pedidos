97aff8928c90fc1c5790529c4560d699acacc63e
DavidCDCB <cristian-cb1@hotmail.com>
2021-02-07 16:04:37 -0500

    informacion

--- vista/js/main.js
+++ vista/js/main.js
@@ -1,18 +1,26 @@
 'use strict';
 
 import dom from './dom-pruebas.js';//clase estatica
-let productos = ["chicle","paleta","gaseosa"];
+let productos = [];
 let mesas = ["1","2","3","4","5","6","7"];
 
-let asignaciones ={
-
-};
+let asignaciones = [];
 
 ((doc, win) => {
 	
 	doc.addEventListener('DOMContentLoaded',event => {
 		document.getElementById("alert").style.display = "none";
 		dom.getElemento("cantidades").value=1;
+
+		peticion("https://raw.githubusercontent.com/DavidCDCB/pedidos/master/productos.json").then(
+			result =>{
+				for (const producto of result) {
+					productos.push(producto["nombre"]+"-"+producto["valor"]);
+				}
+				setItems(doc.querySelector("#productos"),productos);
+				setItems(doc.querySelector("#mesas"),mesas);
+			}
+		);
 
 		dom.getElemento("agregar").addEventListener('click', event => {
 
@@ -22,9 +30,6 @@ let asignaciones ={
 				dom.getElemento("detalles").value,
 			);
 		});
-
-		setItems(doc.querySelector("#productos"),productos);
-		setItems(doc.querySelector("#mesas"),mesas);
 
 		preparacionAsignaciones(asignaciones,mesas);
 		
@@ -40,26 +45,34 @@ let asignaciones ={
 	}
 
 	let asignar=(mesa,producto,cantidad,detalles)=>{
-		asignaciones[mesa]["productos"].push(producto+" x"+cantidad);
-		asignaciones[mesa]["detalles"]=detalles;
+		let total = 0;
+		asignaciones[mesa-1]["productos"].push(producto.split("-")[0]+" x"+cantidad);
+		asignaciones[mesa-1]["detalles"]=detalles;
+
+		for (let index = 0; index < cantidad; index++) {
+			asignaciones[mesa-1]["total"] += parseInt(producto.split("-")[1]);
+		}
+		
 
 		dom.getElemento("texto").innerText= producto+" agregado correctamente a mesa # "+mesa;
-
 		dom.getElemento("alert").style.display = "block";
 		window.setTimeout(function () {
 			dom.getElemento("alert").style.display = "none";
 		}, 3000);
 
+		generarInfo();
 		console.log(asignaciones);
 
 	}
 	
 	let preparacionAsignaciones=(dic,lista)=>{
 		for (const iterator of lista) {
-			dic[iterator] = {
+			dic.push({
+				"mesa":lista.indexOf(iterator)+1,
 				"productos":[],
+				"total":0,
 				"detalles":""
-			};
+			});
 		}
 	}
 
@@ -71,7 +84,26 @@ let asignaciones ={
 	}
 
 	let generarInfo=()=>{
+		let info = "";
 
+		for (const mesas of asignaciones) {
+
+			if(mesas["productos"].length>0){
+				console.log(mesas["mesa"]);
+				info += "MESA #"+mesas["mesa"]+" TOTAL "+mesas["total"]+":\n";
+				for (const producto of mesas["productos"]) {
+					info += "  -> "+producto+"\n";
+					console.log(productos);
+				}
+				if(mesas["detalles"] != "")
+					info += "¡NOTA! "+mesas["detalles"];
+				info += "\n\n";
+			}
+
+		}
+		
+		console.log(info);
+		dom.getElemento("info").value=info;
 	}
 
 	
